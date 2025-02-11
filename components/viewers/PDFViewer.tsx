af1a2d5e2784b18ef517e7df62e976f0d0e18331
GHANIM AL MARZOUQI <optimist_gm@hotmail.com>
2025-02-11 11:56:48 +0400

    update

--- components/viewers/PDFViewer.tsx
+++ components/viewers/PDFViewer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { 
     PdfViewerComponent, 
     Toolbar, 
@@ -24,21 +24,36 @@ interface PDFViewerProps {
 
 export default function PDFViewer({ documentPath }: PDFViewerProps) {
     const [resourceUrl, setResourceUrl] = useState('');
+    const viewerRef = useRef<PdfViewerComponent | null>(null);
 
     useEffect(() => {
         setResourceUrl(`${window.location.protocol}//${window.location.host}/js/ej2-pdfviewer-lib`);
     }, []);
 
+    const handleBeforePrint = () => {
+        setTimeout(() => {
+            const frames = window.frames as unknown as Record<string, Window>;
+            const printWindow = frames["ejs-pdfviewer-print-window"];
+            if (printWindow) {
+                printWindow.focus();
+                printWindow.print();
+            }
+        }, 500);
+    };
+
     if (!resourceUrl) {
         return null; // or a loading state
     }
-    
+
     return (
         <div className='control-section'>
             <PdfViewerComponent 
                 id="container" 
+                ref={viewerRef}
                 documentPath={documentPath}
                 resourceUrl={resourceUrl}
+                printMode="Default"
+                printStart={handleBeforePrint}
                 toolbarSettings={{ 
                     showTooltip : true, 
                     toolbarItems: [
@@ -49,7 +64,7 @@ export default function PDFViewer({ documentPath }: PDFViewerProps) {
                         'PrintOption'
                     ]
                 }}
-                style={{ 'height': '100vh' }}>
+                style={{ height: '100vh' }}>
                 <Inject services={[
                     Toolbar,
                     Magnification,
